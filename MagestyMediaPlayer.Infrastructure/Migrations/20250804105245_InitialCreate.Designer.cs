cb1efcc8463f87503f85100e00cadaea2963a080
A-str0 <il.eroxin@gmail.com>
2025-08-04 13:58:27 +0300

    feature/local-media: add DB migrations

--- MagestyMediaPlayer.Infrastructure/Migrations/20250804105245_InitialCreate.Designer.cs
+++ MagestyMediaPlayer.Infrastructure/Migrations/20250804105245_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MagestyMediaPlayer.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MagestyMediaPlayer.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250804105245_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
